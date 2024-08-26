cbfaae5d32ad331e794b7046b64755d78a204a44
Anna-Karl <112638974+Anna-Karl@users.noreply.github.com>
2024-08-25 23:39:11 -0400

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -8,7 +8,7 @@ set -x
 # > Add your code below each comment to complete the tasks
 
 # 1. Create 5 directories named dir1, dir2, dir3, dir4, and dir5
-mk dir1 dir2 dir3 dir4 dir5
+mkdir dir1 dir2 dir3 dir4 dir5
 
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
 ls -d dir*/
@@ -20,7 +20,7 @@ touch dir2/file1 dir2/file2 dir2/file3 dir2/file4 dir2/file5
 echo "Hello world" >> dir2/file3
 
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
-cat dir2/file4
+cat dir2/file3
 
 # 6. Delete file4
 rm dir2/file4
